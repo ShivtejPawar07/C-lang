4a9cf08f836aeea0617c475bcca4900ba156398f
ShivtejPawar07 <shivtejpawar8@gmail.com>
2024-12-27 07:12:19 +0530

    c

---
+++ fab.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm;
+
+    // Input the number of terms
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: %d, %d", t1, t2);
+
+    // Generate the Fibonacci series
+    for (int i = 3; i <= n; i++) {
+        nextTerm = t1 + t2;
+        printf(", %d", nextTerm);
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    return 0;
+}
